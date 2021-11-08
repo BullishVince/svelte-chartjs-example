fa658699361c6c02cbe913420c3eba34bc1eba27
BullishVince <vincent@bullishlife.com>
2021-11-08 23:13:19 +0100

    init

---
+++ src/routes/chart.svelte
@@ -0,0 +1,36 @@
+<canvas id="myChart" width="3" height="1"></canvas>
+
+<script>
+import {onMount, afterUpdate} from 'svelte';
+
+export let input;
+let chart;
+
+function createChart(chartType, data) {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [1,2,3,4,5,6,7],
+            datasets: [{
+                label: 'My First Dataset',
+                data: input,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        }
+    });
+}
+
+export function addValue(value) {
+    console.log(`update chart with value ${value}`);
+    chart.update();
+}
+
+onMount(createChart);
+afterUpdate(() => {
+    chart.update();
+});
+
+</script>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Chart from './chart.svelte';
+    let chartComponent;
+
+    let settings = {
+        input: [1,2,3,2,12,17,7,2,10,4,24,20]
+    };
+</script>
+
+<h1>Input values below</h1>
+<input type="number" bind:value={settings.input}>
+<button class="inline" on:click={settings.input.push(2)}>Add value</button>
+<Chart bind:this={chartComponent} {...settings}/>
